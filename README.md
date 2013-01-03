a29c7435df33289b65b36eafa53c7fa7c5dc57d2
Sochka Oleksandr <sasha.sochka@gmail.com>
2013-01-03 23:00:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+fenvicktree
+=======
+
+Here is implemented  fenvick tree class in header file and provided example of usage in source file.
+
+Notes: 
+ * used C++11
+ * performed static checker using Google's cpplint.py
---
+++ fenvick.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Oleksandr
+// Copyright   : Licensed with GPLv3
+// Description : Usage example
+//============================================================================
+#include <cstdio>
+#include <functional>
+#include "fenvick.h" // NOLINT
+
+int main() {
+  const int n = 8;
+  FenvickTree<double> fenvickTree(n, 1,
+      std::multiplies<double>(),
+      std::divides<double>());
+  for (int i = 0; i < n; ++i)
+    fenvickTree.set(i, i + 1);
+
+  for (int i = 0; i < n; ++i)
+    for (int j = i; j < n; ++j) {
+      int s = fenvickTree.get(i, j);
+      std::printf(
+          "Function applied from data[%d] to data[%d] is equal to %d \n",
+          i, j, s);
+    }
+  return 0;
+}
